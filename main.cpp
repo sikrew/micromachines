d6ed152efdcec9f2b36748e96ce9345389087914
FRodrigues42 <github@frodrigues.info>
2015-09-26 15:07:00 +0100

    Adicionei uma main esqueleto e um gitignore

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+#if defined(__APPLE__) || defined(MACOSX)
+    #include <GLUT/glut.h>
+#else
+    #include <GL/glut.h>
+#endif
+
+
+using namespace Micromachines;
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+    glutInitWindowSize(800, 562);
+
+    glutCreateWindow("Micromachines");
+
+
+    glutMainLoop();
+    return 0;
+}
+
