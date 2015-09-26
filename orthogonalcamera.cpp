8a0c3fb2501ad6676436647ec8480d79f922fdcb
FRodrigues42 <github@frodrigues.info>
2015-09-26 21:14:39 +0100

    OrthogonalCamera criada

---
+++ orthogonalcamera.cpp
@@ -0,0 +1,28 @@
+#include "orthogonalcamera.h"
+
+using namespace Micromachines;
+
+OrthogonalCamera::OrthogonalCamera(double left, double right, double bottom, double top, double near, double far) : Camera(near, far), _left(left), _right(right), _bottom(bottom), _top(top)
+{
+
+}
+
+OrthogonalCamera::~OrthogonalCamera()
+{
+
+}
+
+void OrthogonalCamera::update()
+{
+
+}
+
+void OrthogonalCamera::computeProjectionMatrix()
+{
+
+}
+
+void OrthogonalCamera::computeVisualizationMatrix()
+{
+
+}
---
+++ orthogonalcamera.h
@@ -0,0 +1,24 @@
+#ifndef ORTHOGONALCAMERA_H
+#define ORTHOGONALCAMERA_H
+
+#include "camera.h"
+
+namespace Micromachines
+{
+class OrthogonalCamera : public Camera
+{
+public:
+    OrthogonalCamera(double left, double right, double bottom, double top, double near, double far);
+    ~OrthogonalCamera();
+    void update();
+    void computeProjectionMatrix();
+    void computeVisualizationMatrix();
+private:
+    double _left;
+    double _right;
+    double _bottom;
+    double _top;
+};
+} // namespace Micromachines
+
+#endif // ORTHOGONALCAMERA_H
