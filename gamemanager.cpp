a1dcf840824c5c6a7681b8950433e330f9447f66
Gil <gil@gilgomes.com>
2015-10-30 11:34:01 +0000

    objectos da mesa já não "voam". Estão em cima do tampo

--- gamemanager.cpp
+++ gamemanager.cpp
@@ -195,48 +195,48 @@ void GameManager::init()
 	_roadside = new Roadside();
 			
 	for (i = 0; i <= 24; i++) 
-		_cheerio[i] = new Cheerio(Vector3(2.8f, -1.98 + i*0.165f, 0.2f));
+		_cheerio[i] = new Cheerio(Vector3(2.8f, -1.98 + i*0.165f, 0.01f));
 
 	for (i = 0; i <= 18; i++) //right left
-		_cheerio[i+25] = new Cheerio(Vector3(2.2f, -1.5 + i*0.165f, 0.2f));
+		_cheerio[i+25] = new Cheerio(Vector3(2.2f, -1.5 + i*0.165f, 0.01f));
 
 	for (i = 0; i <= 25; i++) //left left
-		_cheerio[i+44] = new Cheerio(Vector3(-2.8f, -1.98 + i*0.165f, 0.2f));
+		_cheerio[i + 44] = new Cheerio(Vector3(-2.8f, -1.98 + i*0.165f, 0.01f));
 
 	for (i = 0; i <= 18; i++)//left right
-		_cheerio[i + 70] = new Cheerio(Vector3(-2.2f, -1.5 + i*0.165f, 0.2f));
+		_cheerio[i + 70] = new Cheerio(Vector3(-2.2f, -1.5 + i*0.165f, 0.01f));
 
 	for (i = 0; i <= 24; i++) //bottom top
-		_cheerio[i+89] = new Cheerio(Vector3(-2.0+i*0.168f, -1.95+3*0.154, 0.2f));
+		_cheerio[i + 89] = new Cheerio(Vector3(-2.0 + i*0.168f, -1.95 + 3 * 0.154, 0.01f));
 
 	for (i = 0; i <= 31; i++) //bottom-bottom
-		_cheerio[i+114] = new Cheerio(Vector3(-2.6 + i *0.168f, -2.445 + 3 * 0.154, 0.2f));
+		_cheerio[i + 114] = new Cheerio(Vector3(-2.6 + i *0.168f, -2.445 + 3 * 0.154, 0.01f));
 
 	for (i = 0; i <= 31; i++)//top top
-		_cheerio[i+146] = new Cheerio(Vector3(-2.6 + i*0.168f, 1.52 + 3 * 0.154, 0.2f));
+		_cheerio[i + 146] = new Cheerio(Vector3(-2.6 + i*0.168f, 1.52 + 3 * 0.154, 0.01f));
 
 	for (i = 0; i <= 24; i++) //top bottom
-		_cheerio[i+178] = new Cheerio(Vector3(-2.0 + i*0.168f, 1.0 + 3 * 0.154, 0.2f));
+		_cheerio[i + 178] = new Cheerio(Vector3(-2.0 + i*0.168f, 1.0 + 3 * 0.154, 0.01f));
 		
 		
 
-	_butter[0] = new Butter(new Vector3(2.0,1.8, 0.2));
-	_butter[1] = new Butter(new Vector3(2.0, -1.8, 0.2));
-	_butter[2] = new Butter(new Vector3(-2.0, -1.8, 0.2));
-	_butter[3] = new Butter(new Vector3(-2.0, 1.8, 0.2));
-	_butter[4] = new Butter(new Vector3(0.0, -1.65, 0.2));
+	_butter[0] = new Butter(new Vector3(2.0,1.8, 0.01));
+	_butter[1] = new Butter(new Vector3(2.0, -1.8, 0.01));
+	_butter[2] = new Butter(new Vector3(-2.0, -1.8, 0.01));
+	_butter[3] = new Butter(new Vector3(-2.0, 1.8, 0.01));
+	_butter[4] = new Butter(new Vector3(0.0, -1.65, 0.01));
 
 	float x = gen(rng);
 	float y = gen(rng);
-	_orange[0] = new Orange(Vector3(x, y, 0.2f), gen2(rng));
+	_orange[0] = new Orange(Vector3(x, y, 0.01f), gen2(rng));
 
 	x = gen(rng);
 	y = gen(rng);
-	_orange[1] = new Orange(Vector3(x, y, 0.2f), gen2(rng));
+	_orange[1] = new Orange(Vector3(x, y, 0.01f), gen2(rng));
 
 	x = gen(rng);
 	y = gen(rng);
-	_orange[2] = new Orange(Vector3(x, y, 0.2f), gen2(rng));
+	_orange[2] = new Orange(Vector3(x, y, 0.01f), gen2(rng));
 
 
 	
