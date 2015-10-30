8070bf09226083abf952e813831a56d0adbf9de8
FRodrigues42 <github@frodrigues.info>
2015-10-30 11:32:56 +0000

    Adicionei vectores de colisao

--- gameobject.h
+++ gameobject.h
@@ -12,12 +12,18 @@ public:
     ~GameObject();
     bool getDrawSolidState() const;
     void setDrawSolidState(bool drawSolidState);
+    Vector3 getWSize() const;
+    void setWSize(const Vector3 &wSize);
+    Vector3 getHSize() const;
+    void setHSize(const Vector3 &hSize);
     void toggleDrawSolidState();
     virtual void draw() const = 0;
     virtual void update(double delta_t) = 0;
 
 private:
     bool _drawSolidState;
+    Vector3 _wSize;
+    Vector3 _hSize;
 };
 } // Namespace Micromachines
 
